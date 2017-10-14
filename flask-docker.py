93091ea4dfa8c7d57a9ede7a01cad2a5f27c96a1
Ubuntu <ubuntu@ip-172-31-6-30.ap-northeast-2.compute.internal>
2017-10-14 13:06:02 +0000

    lot changes

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Chanhahahahahged it up a bit!again'
+    return 'Final Chanhahahahahged it up a bit!again'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
